afd3aed963f6ca077e623f4b258c963e507b982f
lijahwhit <elijah@whitti.com>
2022-05-17 20:52:46 -0400

    changes for lesson 4

--- chamber/js/DateAndTime.js
+++ chamber/js/DateAndTime.js
@@ -1,3 +1,23 @@
+
+window.onload=function(){
+
+let hambutton = document.querySelector('.hambutton');
+const mainnav = document.querySelector('.nava')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+
 document.querySelector('#lastupdate').textContent = document.lastModified;
 var year = new Date().getFullYear();
 document.querySelector('#year').textContent = year;
+
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `${fulldate}`;
+}
+
+
+window.onresize = () => {if (window.innerWidth > 1028) mainnav.classList.remove('responsive')};
