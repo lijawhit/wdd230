2469d2f85730e8d44d349ed14ac4faf14fbad2ed
lijahwhit <elijah@whitti.com>
2022-05-25 16:21:02 -0400

    final for week 5 :)

--- chamber/js/DateAndTime.js
+++ chamber/js/DateAndTime.js
@@ -1,25 +1,31 @@
 
 window.onload=function(){
 
-let hambutton = document.querySelector('.hambutton');
-const mainnav = document.querySelector('.nava')
+	const now = new Date();
 
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+	let hambutton = document.querySelector('.hambutton');
+	const mainnav = document.querySelector('.nava')
 
+	hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
-document.querySelector('#lastupdate').textContent = document.lastModified;
-var year = new Date().getFullYear();
-document.querySelector('#year').textContent = year;
-
-const datefield = document.querySelector(".date");
-const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
-);
-datefield.innerHTML = `${fulldate}`;
+	const announcementbanner = document.querySelector(".announcement-banner");
+	if( now.getDay() === 1 || now.getDay() === 2){
+	announcementbanner.style.display = "flex";
+	}else{
+	announcementbanner.style.display = "none";
+	}
 
 
+	document.querySelector('#lastupdate').textContent = document.lastModified;
+	var year = new Date().getFullYear();
+	document.querySelector('#year').textContent = year;
+
+	const datefield = document.querySelector(".date");
+	const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+		now
+	);
+	datefield.innerHTML = `${fulldate}`;
+
+	window.onresize = () => {if (window.innerWidth > 1028) mainnav.classList.remove('responsive')};
 }
 
-
-window.onresize = () => {if (window.innerWidth > 1028) mainnav.classList.remove('responsive')};
