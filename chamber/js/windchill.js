9fa9ba72eeed090e21050a018dc619c2ddf9eac8
lijahwhit <elijah@whitti.com>
2022-05-28 23:11:40 -0400

    windchill update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,7 +1,7 @@
 window.onload=function(){
 
     let temp= 68;
-    let wSpeed= 16;
+    let wSpeed= 10;
     let windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
     
     windChill= Math.round(windChill);
