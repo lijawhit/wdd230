c27e79d322b8980758510e3bac54bf0a833c2b11
lijahwhit <elijah@whitti.com>
2022-06-18 12:34:06 -0400

    windchill update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,11 +1,12 @@
 
-    let temp = Number(document.querySelector(".temp").innerHTML);
-    let wSpeed= Number(document.querySelector(".wind").innerHTML);
-    let windchill = "N/A";
-    if(temp < 50 && wSpeed > 3.0){
-    windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-    windchill= Math.round(windchill);
-    }else{
-    document.getElementById("display").style.display = "none";
-    }
-    document.getElementById("windchill").innerHTML= windchill;
+let temp = Number(document.querySelector(".temp").innerHTML);
+let wSpeed= Number(document.querySelector(".wind").innerHTML);
+let windchill = "N/A";
+if(temp < 50 && wSpeed > 3.0){
+windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+windchill= Math.round(windchill);
+}else{
+document.getElementById("display").style.display = "none";
+}
+document.getElementById("windchill").innerHTML= windchill;
+
