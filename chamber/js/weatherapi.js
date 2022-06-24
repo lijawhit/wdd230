32107ada5a61b70855d50c47cf3ffa544208c333
lijahwhit <elijah@whitti.com>
2022-06-23 22:42:15 -0400

    Using the Weather API

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,39 @@
+  import update_windchill from '../js/windchill.js';
+  
+  // select HTML elements to edit
+  const currentTemp = document.querySelector('#current-temp');
+  
+  const currentWind = document.querySelector('#wind');
+  
+  const weatherIcon = document.querySelector('#weather-icon');
+  const captionDesc = document.querySelector('figcaption');
+  
+  //API URL with arguments  
+  const APIurl = 'https://api.openweathermap.org/data/2.5/weather?q=Williamsburg&appid=4d8f272cf2208705e4404f7c9e164e21&units=imperial';
+
+fetch(APIurl)
+    .then((response) => response.json())
+    .then((weatherinfo) => {
+  
+      console.log(weatherinfo); // this is temporary for development only
+  
+      //set temperature and temperature text color
+      let temp = weatherinfo.main.temp.toFixed(0);
+      currentTemp.innerHTML = `<strong>${temp}</strong>`;
+  
+      currentWind.innerHTML = `<strong>${weatherinfo.wind.speed.toFixed(0)}</strong>`;
+      
+      // weather icon
+      const iconsrc = `https://openweathermap.org/img/w/${weatherinfo.weather[0].icon}.png`;
+      const desc = weatherinfo.weather[0].description;
+  
+      weatherIcon.setAttribute('src', iconsrc);
+      weatherIcon.setAttribute('alt', desc);
+  
+      // weather icon caption
+      captionDesc.textContent = desc;
+  
+      update_windchill();
+  
+    });
+  
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,12 +1,14 @@
+export default function update_windchill(){
 
-let temp = Number(document.querySelector(".temp").innerHTML);
-let wSpeed= Number(document.querySelector(".wind").innerHTML);
-let windchill = "N/A";
-if(temp < 50 && wSpeed > 3.0){
-windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-windchill= Math.round(windchill);
-}else{
-document.getElementById("display").style.display = "none";
+    let temp = Number(document.querySelector("#current-temp").innerHTML);
+    let wSpeed= Number(document.querySelector("#wind").innerHTML);
+    let windchill = "N/A";
+    if(temp < 50 && wSpeed > 3.0){
+    windchill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+    windchill= Math.round(windchill);
+    }else{
+    document.getElementById("display").style.display = "none";
+    }
+    document.getElementById("windchill").innerHTML= windchill;
+
 }
-document.getElementById("windchill").innerHTML= windchill;
-
