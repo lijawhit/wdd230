4476202e669cd5ef9a72cf61aef9f5a1809a76e3
lijahwhit <elijah@whitti.com>
2022-07-16 20:55:55 -0400

    addition of Temple Spotlight

--- TTI&S/js/menu.js
+++ TTI&S/js/menu.js
@@ -14,3 +14,26 @@ let hambutton = document.querySelector('.menu');
 const mainnav = document.querySelector('nav')
 
 menuBtn.addEventListener('click', () => {mainnav.classList.toggle('slide')}, false);
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.querySelector("header");
+var body = document.querySelector("body")
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    body.classList.add("top-padding")
+  } else {
+    header.classList.remove("sticky");
+    body.classList.remove("top-padding")
+  }
+}
